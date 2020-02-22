d3015f584afb5a870d9cda9879bfde88d6b7a072
Burak İbiş <mehmetburakibis@gmail.com>
2020-02-23 02:23:54 +0300

    Add crossorigin tag to manifest link

--- src/components/SEO/index.jsx
+++ src/components/SEO/index.jsx
@@ -154,51 +154,31 @@ const SEO = ({ title, description, image, path, articleDate }) => {
 
   return (
     <Helmet title={seo.title}>
-      <div>
-        <html lang={siteLanguage} />
-        <link rel="canonical" href={`${seo.url}`} />
-        <meta name="description" content={seo.description} />
-        <meta name="image" content={seo.image} />
-        {/* Schema.org */}
-        {!articleDate && <script type="application/ld+json">{JSON.stringify(schemaOrgWebPage)}</script>}
-        {articleDate && <script type="application/ld+json">{JSON.stringify(schemaArticle)}</script>}
-        <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script>
-        {/* OpenGraph */}
-        <meta property="og:site_name" content={defaultTitle} />
-        <meta property="og:url" content={seo.url} />
-        {articleDate && <meta property="og:type" content="article" />}
-        <meta property="og:title" content={seo.title} />
-        <meta property="og:description" content={seo.description} />
-        <meta property="og:image" content={seo.image} />
-        <meta property="og:image:alt" content={seo.description} />
-        {/* fb app id */}
-        <meta property="fb:app_id" content={fbAppId || ''} />
-        {/* Twitter Card */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:creator" content={twitterUsername} />
-        <meta name="twitter:title" content={seo.title} />
-        <meta name="twitter:description" content={seo.description} />
-        <meta name="twitter:image" content={seo.image} />
-        <meta name="twitter:image:alt" content={seo.description} />
-
-        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#ffffff" />
-      </div>
+      <html lang={siteLanguage} />
+      <link rel="canonical" href={`${seo.url}`} />
+      <meta name="description" content={seo.description} />
+      <meta name="image" content={seo.image} />
+      {/* Schema.org */}
+      {!articleDate && <script type="application/ld+json">{JSON.stringify(schemaOrgWebPage)}</script>}
+      {articleDate && <script type="application/ld+json">{JSON.stringify(schemaArticle)}</script>}
+      <script type="application/ld+json">{JSON.stringify(breadcrumb)}</script>
+      {/* OpenGraph */}
+      <meta property="og:site_name" content={defaultTitle} />
+      <meta property="og:url" content={seo.url} />
+      {articleDate && <meta property="og:type" content="article" />}
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:description" content={seo.description} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="og:image:alt" content={seo.description} />
+      {/* fb app id */}
+      <meta property="fb:app_id" content={fbAppId || ''} />
+      {/* Twitter Card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:creator" content={twitterUsername} />
+      <meta name="twitter:title" content={seo.title} />
+      <meta name="twitter:description" content={seo.description} />
+      <meta name="twitter:image" content={seo.image} />
+      <meta name="twitter:image:alt" content={seo.description} />
     </Helmet>
   );
 };
