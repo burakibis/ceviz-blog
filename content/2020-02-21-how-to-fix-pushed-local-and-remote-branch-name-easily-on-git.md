c95a020e259f29850a7e04c83313b5bbb9145080
Burak İbiş <mehmetburak@artistanbul.io>
2020-03-16 02:09:32 +0300

    Fix wrapping and fork new fixes

--- content/2020-02-21-how-to-fix-pushed-local-and-remote-branch-name-easily-on-git.md
+++ content/2020-02-21-how-to-fix-pushed-local-and-remote-branch-name-easily-on-git.md
@@ -1,23 +1,22 @@
 ---
-title: How to fix pushed local and remote branch name easily on Git?
-date: 2020-01-24T09:56:00.000Z
-cover: /images/ezgif.com-webp-to-jpg.jpg
-category: Tech Hecks
+title: "How to fix pushed local and remote branch name easily on Git?"
+date: "2020-01-24T09:56:00.000Z"
+category: "Tech Hecks"
+cover: /images/git.png
 tags:
   - git
   - version control system
 ---
+
 You created branch and write some of codes, pushed to remote but suddenly you noticed that the branch name was incorrect! Ahh.. Don't worry, you can change easily your branch name:
 
-* Firstly change local branch name on your branch: 
-
+- Firstly change local branch name on your branch:
 
 ```
 git branch -m new-name
 ```
 
-* Push that to remote:
-
+- Push that to remote:
 
 ```
 git push origin :old-name new-name
@@ -27,12 +26,11 @@ Your old shitty branch and merge request will delete and you can open new merge 
 
 ## Here are some Git commands that will help:
 
-### Getting & Creating Projects
-
-| Command                                                           | Description                                |
-| ----------------------------------------------------------------- | ------------------------------------------ |
-| `git init`                                                        | Initialize a local Git repository          |
-| `git clone ssh://git@github.com/[username]/[repository-name].git` | Create a local copy of a remote repository |
+br = branch name
+sb = source branch
+tb = target branch
+obr = old branch name
+nbr = new branch name
 
 ### Basic Snapshotting
 
@@ -46,45 +44,43 @@ Your old shitty branch and merge request will delete and you can open new merge 
 
 ### Branching & Merging
 
-| Command                                              | Description                                             |
-| ---------------------------------------------------- | ------------------------------------------------------- |
-| `git branch`                                         | List branches (the asterisk denotes the current branch) |
-| `git branch -a`                                      | List all branches (local and remote)                    |
-| `git branch [branch name]`                           | Create a new branch                                     |
-| `git branch -d [branch name]`                        | Delete a branch                                         |
-| `git push origin --delete [branch name]`             | Delete a remote branch                                  |
-| `git checkout -b [branch name]`                      | Create a new branch and switch to it                    |
-| `git checkout -b [branch name] origin/[branch name]` | Clone a remote branch and switch to it                  |
-| `git branch -m [old branch name] [new branch name]`  | Rename a local branch                                   |
-| `git checkout [branch name]`                         | Switch to a branch                                      |
-| `git checkout -`                                     | Switch to the branch last checked out                   |
-| `git checkout -- [file-name.txt]`                    | Discard changes to a file                               |
-| `git merge [branch name]`                            | Merge a branch into the active branch                   |
-| `git merge [source branch] [target branch]`          | Merge a branch into a target branch                     |
-| `git stash`                                          | Stash changes in a dirty working directory              |
-| `git stash clear`                                    | Remove all stashed entries                              |
+| Command                            | Description                                             |
+| ---------------------------------- | ------------------------------------------------------- |
+| `git branch`                       | List branches (the asterisk denotes the current branch) |
+| `git branch -a`                    | List all branches (local and remote)                    |
+| `git branch [br]`                  | Create a new branch                                     |
+| `git branch -d [br]`               | Delete a branch                                         |
+| `git push origin -D [br]`          | Delete a remote branch                                  |
+| `git checkout -b [br]`             | Create a new branch and switch to it                    |
+| `git checkout -b [br] origin/[br]` | Clone a remote branch and switch to it                  |
+| `git branch -m [obr] [nbr]`        | Rename a local branch                                   |
+| `git checkout [br]`                | Switch to a branch                                      |
+| `git checkout -`                   | Switch to the branch last checked out                   |
+| `git checkout -- [file-name.txt]`  | Discard changes to a file                               |
+| `git merge [br]`                   | Merge a branch into the active branch                   |
+| `git merge [sb] [tb]`              | Merge a branch into a target branch                     |
+| `git stash`                        | Stash changes in a dirty working directory              |
+| `git stash clear`                  | Remove all stashed entries                              |
 
 ### Sharing & Updating Projects
 
-| Command                                                                           | Description                                                 |
-| --------------------------------------------------------------------------------- | ----------------------------------------------------------- |
-| `git push origin [branch name]`                                                   | Push a branch to your remote repository                     |
-| `git push -u origin [branch name]`                                                | Push changes to remote repository (and remember the branch) |
-| `git push`                                                                        | Push changes to remote repository (remembered branch)       |
-| `git push origin --delete [branch name]`                                          | Delete a remote branch                                      |
-| `git pull`                                                                        | Update local repository to the newest commit                |
-| `git pull origin [branch name]`                                                   | Pull changes from remote repository                         |
-| `git remote add origin ssh://git@github.com/[username]/[repository-name].git`     | Add a remote repository                                     |
-| `git remote set-url origin ssh://git@github.com/[username]/[repository-name].git` | Set a repository's origin branch to SSH                     |
+| Command                         | Description                                                 |
+| ------------------------------- | ----------------------------------------------------------- |
+| `git push origin [br]`          | Push a branch to your remote repository                     |
+| `git push -u origin [br]`       | Push changes to remote repository (and remember the branch) |
+| `git push`                      | Push changes to remote repository (remembered branch)       |
+| `git push origin --delete [br]` | Delete a remote branch                                      |
+| `git pull`                      | Update local repository to the newest commit                |
+| `git pull origin [br]`          | Pull changes from remote repository                         |
 
 ### Inspection & Comparison
 
-| Command                                    | Description                    |
-| ------------------------------------------ | ------------------------------ |
-| `git log`                                  | View changes                   |
-| `git log --summary`                        | View changes (detailed)        |
-| `git log --oneline`                        | View changes (briefly)         |
-| `git diff [source branch] [target branch]` | Preview changes before merging |
+| Command              | Description                    |
+| -------------------- | ------------------------------ |
+| `git log`            | View changes                   |
+| `git log --summary`  | View changes (detailed)        |
+| `git log --oneline`  | View changes (briefly)         |
+| `git diff [sb] [tb]` | Preview changes before merging |
 
 _[Sources:_ [_https://www.github.com/joshnh/Git-Commands/blob/master/README.md_](https://github.com/joshnh/Git-Commands/blob/master/README.md)_]_
 
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Layout from '../layout';
-import profile from '../../static/profile.png';
-import SEO from '../components/SEO';
-import './about.scss';
+import React from "react";
+import Layout from "../layout";
+import profile from "../../static/profile.png";
+import SEO from "../components/SEO";
+import "./about.scss";
 
 const About = () => {
   return (
@@ -12,20 +12,38 @@ const About = () => {
         <img src={profile} className="user-avatar" alt="profile" />
         <section className="about-context">
           <div>
-            <h1>About Ceviz</h1>
+            <h1>About Ceviz Lab</h1>
             <p>
-              <strong>ceviz.io</strong> is a technology blog where I mostly share my software experiences, problems and
-              projects I encounter on a weekly basis. It is also a small contribution to the open source world. All
-              kinds of questions, opinions, suggestions, even 'I have an idea but..' everything you say. Feel free to
-              contact with me on my e-mail (burak@ceviz.io).
+              Ceviz Lab is a roof where I gather my initiatives.{" "}
+              <strong>ceviz.io</strong> is a technology blog where I mostly
+              share my software experiences, problems and projects I encounter
+              on a weekly basis. It is also a small contribution to the open
+              source world. All kinds of questions, opinions, suggestions, even
+              'I have an idea but..' everything you say. Feel free to contact
+              with me on my e-mail, Twitter or Instagram.
             </p>
-            <h2 id="personalinformations">Personal Informations</h2>
+            <h1>About Me</h1>
             <p>
-              I was born in Balikesir/Turkey at 1993 of July. In 1998, my father bought a computer that name is
-              "Escort". I learnt using PC with Chip and PCOyun magazines. The first website that I entered with ixir and
-              56K modem (that's my first internet provider) is grafi2000. When we realized that the internet was
-              something that could end, I unplugged it and played flash games with offline mode of Internet Explorer.
+              I was born in Balikesir/Turkey at 1993 of July. In 1998, my father
+              bought a computer that name is "Escort". I learnt using PC with
+              Chip and PCOyun magazines. The first website that I entered with
+              ixir and 56K modem (that's my first internet provider) is
+              grafi2000. When we realized that the internet was something that
+              could end, I unplugged it and played flash games with offline mode
+              of Internet Explorer.
             </p>
+            <br />
+            <p>
+              I love open-source software that is why I am currently using Linux
+              and open source applications. In my personal computer, I am using
+              Fedora 30. Also I love contributing to open-source projects. So If
+              you have any idea, please contact me immediately!
+            </p>
+            <ul>
+              <li>
+                
+              </li>
+            </ul>
             <h2 id="jobexperiences">Job Experiences</h2>
             <ul>
               <li>2015-2016 - Zorlu Holding - Budget Reporting Specialist</li>
@@ -40,19 +58,23 @@ const About = () => {
               <li>Javascript based: React.js, Gatsby.js, React Native,</li>
               <li>DevOps: Vagrant, Docker,</li>
               <li>Database: PostgreSQL, MySQL,</li>
-              <li>Tools: GitLab (+API), GitHub (+API), Redmine (+API), Sendgrid (+API), Slack (I ❤ Slack API)</li>
+              <li>
+                Tools: GitLab (+API), GitHub (+API), Redmine (+API), Sendgrid
+                (+API), Slack (I ❤ Slack API)
+              </li>
               <li>Trying something with Arduino</li>
             </ul>
             <h1 id="projects">Projects</h1>
             <h2 id="yanci-tracker">Yanci Tracker</h2>
             <img
-              style={{ width: '500px' }}
+              style={{ width: "500px" }}
               src="https://res.cloudinary.com/dqdghe6rx/image/upload/v1582248999/ceviz.io/screencapture-yanci-herokuapp-2020-02-21-01_42_34-min_lkxdf1.png"
             />
             <hr />
             <p>
-              Food order tracking application for companies. It was written to track the debtors that disappeared after
-              ordering, to remind them of their debts, to report who owes how much.
+              Food order tracking application for companies. It was written to
+              track the debtors that disappeared after ordering, to remind them
+              of their debts, to report who owes how much.
             </p>
             <ul>
               <strong>Stacks:</strong>
@@ -80,13 +102,13 @@ const About = () => {
             </ul>
             <h2 id="gratis-career">Gratis Career</h2>
             <img
-              style={{ width: '500px' }}
+              style={{ width: "500px" }}
               src="https://res.cloudinary.com/dqdghe6rx/image/upload/v1582249022/ceviz.io/screencapture-kariyer-gratis-2020-02-21-01_42_59-min_kxhre5.png"
             />
             <hr />
             <p>
-              Gratis’s corporate and career website. Includes job applications, employee interviews, information about
-              the company.
+              Gratis’s corporate and career website. Includes job applications,
+              employee interviews, information about the company.
             </p>
             <ul>
               <li>
@@ -98,7 +120,10 @@ const About = () => {
               </li>
             </ul>
             <h2 id="badpanty-e-commerce">Badpanty E-Commerce</h2>
-            <p>C2C e-commerce site that supports exchanges with Cryptocurrency. Customers sell to customers.</p>
+            <p>
+              C2C e-commerce site that supports exchanges with Cryptocurrency.
+              Customers sell to customers.
+            </p>
             <ul>
               <li>
                 <strong>Stacks:</strong>
@@ -111,7 +136,9 @@ const About = () => {
                 <p>Django REST Framework 3</p>
               </li>
               <li>
-                <p>React.js 16.1 + Jinja2 (two app on server with webpack bundle)</p>
+                <p>
+                  React.js 16.1 + Jinja2 (two app on server with webpack bundle)
+                </p>
               </li>
               <li>
                 <p>PostgreSQL</p>
@@ -125,9 +152,10 @@ const About = () => {
             </ul>
             <h2 id="hotnews-mobile-app">HotNews Mobile App</h2>
             <p>
-              HotNews is mobile news application. The news shared from the user interface are sent as a notification to
-              the users’ mobile phones upon request. It has the features of showing news according to the location,
-              recording the news, commenting on the news.
+              HotNews is mobile news application. The news shared from the user
+              interface are sent as a notification to the users’ mobile phones
+              upon request. It has the features of showing news according to the
+              location, recording the news, commenting on the news.
             </p>
             <ul>
               <li>
@@ -152,16 +180,19 @@ const About = () => {
             </ul>
             <h2 id="ceviz.io-blog">ceviz.io Blog</h2>
             <p>
-              A personal blog that open-source where I share the problems I often encounter and the new technologies I
-              learn. I used Papan01 themes on my site. If you are wondering how to install, deploy and improve, here
-              url:{' '}
+              A personal blog that open-source where I share the problems I
+              often encounter and the new technologies I learn. I used Papan01
+              themes on my site. If you are wondering how to install, deploy and
+              improve, here url:{" "}
               <a href="https://github.com/papan01/gatsby-starter-papan01">
                 https://github.com/papan01/gatsby-starter-papan01
               </a>
             </p>
             <p>
-              GitHub Repo:{' '}
-              <a href="https://github.com/mindlabcore/ceviz-blog">https://github.com/mindlabcore/ceviz-blog</a>
+              GitHub Repo:{" "}
+              <a href="https://github.com/mindlabcore/ceviz-blog">
+                https://github.com/mindlabcore/ceviz-blog
+              </a>
             </p>
             <ul>
               <li>
@@ -186,11 +217,13 @@ const About = () => {
             </ul>
             <h2 id="virgul.co-blog">virgul.co Blog</h2>
             <p>
-              The website that open-source and based on Jekyll (Ruby) where I shared my blog posts in the past. Content
-              management is troublesome, has a lot of bugs. Therefore, it is no longer available.
+              The website that open-source and based on Jekyll (Ruby) where I
+              shared my blog posts in the past. Content management is
+              troublesome, has a lot of bugs. Therefore, it is no longer
+              available.
             </p>
             <p>
-              GitHub Repo:{' '}
+              GitHub Repo:{" "}
               <a href="https://github.com/virgulco/virgulco.github.io">
                 https://github.com/virgulco/virgulco.github.io
               </a>
@@ -215,14 +248,15 @@ const About = () => {
             </ul>
             <h2 id="hello-little-arduino">Hello Little Arduino</h2>
             <p>
-              It is a simple remote control system project with http socket using a simple Arduino Uno and a few leds.
-              You can find my blog post, the details of which can be found at the following link:{' '}
+              It is a simple remote control system project with http socket
+              using a simple Arduino Uno and a few leds. You can find my blog
+              post, the details of which can be found at the following link:{" "}
               <a href="https://www.ceviz.io/archives/2019-12-20-testing-in-netlify-cms">
                 https://www.ceviz.io/archives/2019-12-20-testing-in-netlify-cms
               </a>
             </p>
             <p>
-              GitLab Repo:{' '}
+              GitLab Repo:{" "}
               <a href="https://gitlab.com/burakibis/hello-little-arduino">
                 https://gitlab.com/burakibis/hello-little-arduino
               </a>
